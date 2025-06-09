b50762937728849b14b4824ded0ee96168f6fc12
SaiAshwathaSingari <singarisai777@gamil.com>
2025-06-09 15:30:49 +0530

    Before pushing on git

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const {userRoute} = require('./routes/usersRouter')
 const {hostRouter} = require('./routes/hostRouter');
 const {errorHandlingPage} = require('./routes/errorhandlingpage');
 const rootdir = require('./utils/pathUtil');
+const path = require('path');
 const app = express();
 
 
@@ -16,7 +17,9 @@ app.use(express.urlencoded());
 
 app.use(userRoute);
 app.use(hostRouter);
+app.use(express.static(path.join(rootdir,'public')));
 app.use(errorHandlingPage);
+
 app.listen(port, ()=>{
   console.log(`Server is running on http://localhost:${port}`);
 })
--- routes/errorhandlingpage.js
+++ routes/errorhandlingpage.js
@@ -3,9 +3,10 @@ const path = require('path');
 const rootdir = require('../utils/pathUtil');
 
 const errorHandlingPage = express.Router();
+
 errorHandlingPage.use((req,res,next)=>{
   
-  res.status(404).sendFile(path.join(rootdir,'../views/error.html'));
+  res.status(404).sendFile(path.join(rootdir,'./views/error.html'));
 });
 
 exports.errorHandlingPage = errorHandlingPage;
